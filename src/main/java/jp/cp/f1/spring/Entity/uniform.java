a74566d6ced19e79cab82be6178dd0534021095a
TaokaRyouga <taoka@gmail.com>
2026-06-22 16:07:57 +0900

    test

--- src/main/java/jp/cp/f1/spring/Entity/uniform.java
+++ src/main/java/jp/cp/f1/spring/Entity/uniform.java
@@ -1,5 +1,13 @@
 package jp.cp.f1.spring.Entity;
 
+
+@Entity
+@Table(name="uniforminfo")
 public class uniform {
 
+	@Id
+	@Column(length = 20)
+	private String uniid;
+	
+	
 }
--- src/main/java/jp/cp/f1/spring/Entity/user.java
+++ src/main/java/jp/cp/f1/spring/Entity/user.java
@@ -1,5 +1,12 @@
 package jp.cp.f1.spring.Entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Etity
+@Table(name="userinfo")
 public class user {
 
 }
