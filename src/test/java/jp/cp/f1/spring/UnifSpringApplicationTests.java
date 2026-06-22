8502d2f7946ebe1c9a33b4a0fe1d0d864d99f63d
Yagihara <Yagihara@exmail.com>
2026-06-22 14:06:22 +0900

    First Commit

---
+++ src/test/java/jp/cp/f1/spring/UnifSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.cp.f1.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnifSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
