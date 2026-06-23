7240c765f83167df66a69b1cce9561936da91b71
TaokaRyouga <taoka@gmail.com>
2026-06-23 09:15:11 +0900

    ウニフォームDTOの作成

--- src/main/java/jp/co/f1/spring/Entity/Uniform.java
+++ src/main/java/jp/co/f1/spring/Entity/Uniform.java
@@ -1,5 +1,56 @@
 package jp.co.f1.spring.Entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="uniforminfo")
 public class Uniform {
 
+	@Id
+	@Column(length = 8)
+	private String uniid;
+	
+	@Column(length = 20)
+	private String uniname;
+	
+	private Integer price;
+	
+	private Integer stook;
+
+	public String getUniid() {
+		return uniid;
+	}
+
+	public void setUniid(String uniid) {
+		this.uniid = uniid;
+	}
+
+	public String getUniname() {
+		return uniname;
+	}
+
+	public void setUniname(String uniname) {
+		this.uniname = uniname;
+	}
+
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+
+	public Integer getStook() {
+		return stook;
+	}
+
+	public void setStook(Integer stook) {
+		this.stook = stook;
+	}
+	
+	
 }
