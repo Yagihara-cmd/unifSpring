73b090ff0f7481dd5dda866ec9557c66a48e7dd1
Tsukada <k-tsukada@cries.co.jp>
2026-06-24 15:41:59 +0900

    GuestPurchaseConfirmとOrderDtailを追加

---
+++ src/main/java/jp/co/f1/spring/UniController/GuestPurchaseConfirm.java
@@ -0,0 +1,135 @@
+/*
+ * 
+ *  ゲストの購入品確認画面
+ * 
+ *  担当:塚田
+ *  最終更新:2026/06/24-PM15
+ * 
+ * 
+ */
+
+package jp.co.f1.spring.UniController;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSendException;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import jp.co.f1.spring.Entity.Order;
+import jp.co.f1.spring.Entity.Uniform;
+import jp.co.f1.spring.Entity.User;
+import jp.co.f1.spring.Repository.OrderRepository;
+import jp.co.f1.spring.Repository.UniRepository;
+
+@Controller
+public class GuestPurchaseConfirm {
+
+	@Autowired
+	private UniRepository uniforminfo;
+	
+	@Autowired
+	private OrderRepository orderinfo;
+	
+	@Autowired
+	private HttpSession session;
+	
+	@Autowired
+	private MailSender mailSender;
+	
+	public static final String LINE_SEPARATOR = System.getProperty("line.separator");
+	
+	//「/guestPurchaseConfirm」へGET送信された場合
+	@GetMapping("/guestPurchaseConfirm")
+	public ModelAndView guestPurchaseConfirm(HttpServletRequest request, ModelAndView mav) {
+	
+		//ゲストのメールアドレスを画面から取得
+		String guestEmail = request.getParameter("guestEmail");
+		String guestName = request.getParameter("guestName");
+		
+		//セッションからカート情報を取得
+		ArrayList<Order> order_list = (ArrayList<Order>) session.getAttribute("order_list");
+		
+		//ユニフォーム一覧
+		ArrayList<Uniform> uni_list = new ArrayList<>();
+		
+		//カートの中身がない場合
+		if (order_list == null) {
+			mav.addObject("errorMessage", "カートの中に何も無かったので購入は出来ません。");
+			mav.addObject("cmd", "menu");
+			mav.addObject("next", "[ゲスト情報入力画面へ戻る]");
+			mav.setViewName("view/users/guestDateInput");
+		return mav;
+		}
+		
+		//合計金額用変数の初期化
+		int total = 0;
+		
+		//オーダーリストから1件ずつ取り出す
+		for (Order order : order_list) {
+		
+			//注文情報をDBに保存
+			orderinfo.saveAndFlush(order);
+		
+			//ユニフォーム情報を取得
+			Optional<Uniform> uniList = uniforminfo.findByUniNo(order.getUniid());
+		
+			//合計を計算する
+			total += Uniform.getPrice();
+		
+			uni_list.add(Uniform);
+		
+		}
+		
+		//Viewに渡す変数をModelに格納
+		mav.addObject("total", total);
+		mav.addObject("uni_list", uni_list);
+		
+		//メール送信
+		try {
+			SimpleMailMessage msg = new SimpleMailMessage();
+			
+			msg.setFrom(guestEmail);
+			
+			String insertMessage = guestName + "様" + LINE_SEPARATOR + LINE_SEPARATOR;
+			insertMessage += "ユニフォームのご購入ありがとうございます。" + LINE_SEPARATOR;
+			insertMessage += "以下内容でご注文を受け付けましたので、ご連絡いたします。"
+			+ LINE_SEPARATOR + LINE_SEPARATOR;
+			
+			for (int j = 0; j < uni_list.size(); j++) {
+				Uniform uni = uni_list.get(j);
+				insertMessage += uni.getUniid() + "\t" + uni.getUniname() + "\t" + uni.getPrice() + "円" + ""+ LINE_SEPARATOR;
+			}
+			
+			insertMessage += "合計" + total + "円" + LINE_SEPARATOR + LINE_SEPARATOR;
+			insertMessage += "ご利用ありがとうございました。" + LINE_SEPARATOR;
+			
+			msg.setSubject("ユニフォーム購入情報");
+			msg.setText(insertMessage);
+			mailSender.send(msg);
+		
+		} catch (MailSendException e) {
+			mav.addObject("errorMessage", "メールの送信ができませんでした。");
+			mav.addObject("cmd", "menu");
+			mav.addObject("next", "[ゲスト情報入力画面へ]");
+			mav.setViewName("view/users/guestDateInput");
+			return mav;
+		}
+		
+		//セッション情報削除
+		session.removeAttribute("order_list");
+		
+		//画面に出力するViewを指定
+		mav.setViewName("view/user/guestPurchaseConfirm");
+		
+		return mav;
+		}
+}
--- src/main/java/jp/co/f1/spring/UniController/OrderDetail.java
+++ src/main/java/jp/co/f1/spring/UniController/OrderDetail.java
@@ -1,5 +1,63 @@
+/*
+ * 
+ *  受注商品の詳細
+ * 
+ *  担当:塚田
+ *  最終更新:2026/06/24-PM15
+ * 
+ * 
+ */
+
 package jp.co.f1.spring.UniController;
 
+import java.util.Optional;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import jp.co.f1.spring.Entity.Order;
+import jp.co.f1.spring.Repository.OrderRepository;
+import jp.co.f1.spring.Repository.UniRepository;
+
+@Controller
 public class OrderDetail {
 
+   @Autowired
+   private OrderRepository orderRepository;
+
+   // 「/orderDetail」へアクセスがあった場合
+   @GetMapping("/orderDetail")
+   public ModelAndView orderDetail(HttpServletRequest request, ModelAndView mav) {
+
+       //HTML側からもらった注文Noを取得
+       String orderNo = request.getParameter("orderNo");
+
+       //注文Noをもとに注文情報を検索
+       Optional<Order> optionalOrder = orderRepository.findByOrderNo(orderNo);
+
+       //注文情報が存在しない場合
+       if (!(optionalOrder.isPresent())) {
+
+           mav.addObject("errorMessage", "表示対象の注文情報が存在しない為、詳細情報は表示出来ませんでした。");
+           mav.addObject("cmd", "orderList");
+           mav.addObject("next", "[受注管理一覧へ戻る]");
+
+           //画面に出力するViewを指定
+           mav.setViewName("view/users/OrderManagementList");
+
+           return mav;
+       }
+
+       //Viewに渡す変数をModelに格納
+       mav.addObject("order", optionalOrder.get());
+
+       //画面に出力するViewを指定
+       mav.setViewName("view/orderDetail");
+
+       return mav;
+   }
 }
