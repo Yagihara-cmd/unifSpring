1bd3634fc0976095d3d492ae3c3452e96c16f6cc
Yagihara <Yagihara@exmail.com>
2026-06-25 17:59:59 +0900

    0625のプッシュ

--- src/main/java/jp/co/f1/spring/UniController/UserOrderCreate.java
+++ src/main/java/jp/co/f1/spring/UniController/UserOrderCreate.java
@@ -76,21 +76,14 @@ public class UserOrderCreate {
 
 		//セッションからUserの値を取得・セッション切れならばエラーに遷移
 		User user = (User) session.getAttribute("user");
-		
-		
-		
-		
-
-		
 
 		//セッションからOrderListを取得する
 		ArrayList<Order> order_list = new ArrayList<Order>();
-        if (session.getAttribute("order_list") != null) {
-            order_list = (ArrayList<Order>) session.getAttribute("order_list");
-        }
-		
-		
-		
+
+		if ((ArrayList<Order>) session.getAttribute("order_list") != null) {
+			order_list = (ArrayList<Order>) session.getAttribute("order_list");
+		}
+
 		//userのセッションがある場合
 		if (user == null) {
 			//セッション切れ
@@ -101,59 +94,76 @@ public class UserOrderCreate {
 			return mav;
 		}
 
-		//uniidのパラメータを取得し詳細を取得,オブジェクトに格納
-		Optional<Uniform> uniform = uniforminfo.findByUniid(request.getParameter("uniid"));
-
-		//order情報をOrderに格納するためのオブジェクト宣言
-		Order order = new Order();
-
-		//uniid
-		order.setUniid(request.getParameter("uniid"));
-
-		//userid	
-		order.setUserid("user.userid");
-
-		//数量を格納
-		//order.setQuantity(Integer.parseInt(request.getParameter("quantity")));
-
-		//数量を1に固定
-		order.setQuantity(1);
-
-		//時刻を取得
-		Date date = new Date();
-		order.setDate(date);
-
-		//発送状況、入金状況の設定
-		order.setShippingstatus("0");
-
-		order.setPaymentstatus("0");
-
-		//OrderListにorderのオブジェクトを追加する
-		order_list.add(order);
-
-		
-
-		
-		int total = 0;
-
-		//ArrayList<Uniform> uniform_list = new ArrayList<Uniform>();
-
-		//合計金額の算出
 		//カウント変数の宣言
-		//int j = 0;
-		//
-		//		for (Order order : order_list) {
-		//			Optional<Uniform> optionalUniform = uniforminfo.findByUniid(order.getUniid());
-		//			Uniform uniform = optionalUniform.get(); //uniformオブジェクトで受け取っておくと金額計算の際に可読性が上がる
-		//			uniform_list.add(uniform);
-		//
-		//			//total += Integer.parseInt(uniform_list.get(j).getPrice()) * order.getQuantity();
-		//
-		//			total += uniform_list.get(j).getPrice() * order.getQuantity();
-		//			j++;
-		//		}
+		int i = 0;
 
-		session.setAttribute("order_list", order_list);
+		Order order2 = new Order();
+
+		if (request.getParameter("delno") != null) { //delnoの値がある場合
+			// orderList全件から"delno"と同じISBNの値が見つかるまで繰り返す
+
+			Loop: while (i < order_list.size()) {
+				order2 = order_list.get(i);
+
+				if (order2.getUniid().equals(request.getParameter("delno"))) { //isbnとdelnoが一致したとき
+
+					break Loop;
+
+				}
+				i++;
+			}
+			order_list.remove(order_list.indexOf(order2));
+			mav = new ModelAndView("redirect:/UserOrderCreate");
+			return mav;
+		}
+
+		if (request.getParameter("uniid") != null) {
+			//uniidのパラメータを取得し詳細を取得,オブジェクトに格納
+			Optional<Uniform> uniform = uniforminfo.findByUniid(request.getParameter("uniid"));
+
+			//order情報をOrderに格納するためのオブジェクト宣言
+			Order order = new Order();
+
+			//uniid
+			order.setUniid(request.getParameter("uniid"));
+
+			//userid	
+			order.setUserid(user.getUserid());
+
+			//数量を格納
+			//order.setQuantity(Integer.parseInt(request.getParameter("quantity")));
+
+			//数量を1に固定
+			order.setQuantity(1);
+
+			//時刻を取得
+			Date date = new Date();
+			order.setDate(date);
+
+			//発送状況、入金状況の設定
+			order.setShippingstatus("0");
+
+			order.setPaymentstatus("0");
+
+			//OrderListにorderのオブジェクトを追加する
+			order_list.add(order);
+
+			int total = 0;
+			int j = 0;
+			ArrayList<Uniform> uniform_list = new ArrayList<Uniform>();
+
+			for (Order order1 : order_list) {
+				Optional<Uniform> optionalUniform = uniforminfo.findByUniid(order.getUniid());
+				Uniform uniform1 = optionalUniform.get(); //uniformオブジェクトで受け取っておくと金額計算の際に可読性が上がる
+				uniform_list.add(uniform1);
+				total += uniform_list.get(j).getPrice() * order.getQuantity();
+				j++;
+			}
+			session.setAttribute("order_list", order_list);
+
+			mav.addObject("total", total);
+		}
+
 
 		//mav.addObject("uniform_list", uniform_list);
 		mav.addObject("order_list", order_list);
@@ -165,6 +175,7 @@ public class UserOrderCreate {
 		 * 
 		 */
 		mav.setViewName("view/users/userOrderCreate");
+
 		return mav;
 	}
 }
